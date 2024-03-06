18f5a0e24cefc5bb8f5e1fb525c8fb7435059b6a
Murilo Penha <murilo.penha@fraga.com.br>
2024-03-06 15:11:02 -0300

    roteamento das páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ExibicaoUsuariosComponent } from './pages/exibicao-usuarios/exibicao-usuarios.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'usuarios', component: ExibicaoUsuariosComponent },
+  {
+    path: 'usuarios',
+    component: ExibicaoUsuariosComponent,
+    canActivate: [authGuard],
+  },
 ];
