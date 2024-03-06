79d1a15b41d9d35f69d063b746c56b2d7a00080c
Murilo Penha <murilo.penha@fraga.com.br>
2024-03-06 09:53:55 -0300

    roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ExibicaoUsuariosComponent } from './pages/exibicao-usuarios/exibicao-usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'usuarios', component: ExibicaoUsuariosComponent },
+];
